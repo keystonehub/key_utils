fafa83d7f7a77b6493023630a3ba6291779a8178
Case <90377400+CaseIRL@users.noreply.github.com>
2025-02-19 16:50:46 +0000

    Update server.lua
    removed category takes from prints dont want to tag all the old stuff.

--- modules/bridges/notifications/server.lua
+++ modules/bridges/notifications/server.lua
@@ -4,11 +4,11 @@ local notifications = {}
 --- @param source number: The ID of the target player.
 --- @param options table: The notification options (type, message, header, duration).
 local function notify(source, options)
-    if not source or not options or not options.type or not options.message then
-        utils.debug_log('error', '[Notifications] Invalid parameters for notify function.')
+    if not source or not options or not (options.type and options.message) then
+        utils.debug_log('error', 'Invalid parameters for notify function.')
         return
     end
-    local cache_id = ('%s_%s'):format(source, options.message)
+    local cache_id = source .. "_" .. options.message
     if notifications[cache_id] then return end
     notifications[cache_id] = true
     TriggerClientEvent('fivem_utils:cl:send_notification', source, options)
