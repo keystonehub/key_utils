92e6b8a91c3ab1b83959164d3a3c020104a0cbd9
Case <90377400+CaseIRL@users.noreply.github.com>
2025-02-20 04:50:05 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,7 @@
 |_|\_\_____| |_| |____/ |_| \___/|_| \_|_____|
 ----------------------------------------------                                               
            Developer Utility Library
-                   V1.0.7             
+                   V1.0.8             
 ----------------------------------------------
 ]]
 
@@ -15,7 +15,7 @@ fx_version 'cerulean'
 games { 'gta5', 'rdr3' }
 
 name 'fivem_utils'
-version '1.0.7'
+version '1.0.8'
 description 'Keystone - Developer Utility Library'
 author 'keystone'
 repository 'https://github.com/keystonehub/fivem_utils'
