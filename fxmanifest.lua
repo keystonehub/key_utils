5ad559c78b8ae743cecb00f91a2ce9f1fd8f423c
Case <90377400+CaseIRL@users.noreply.github.com>
2025-03-16 22:35:03 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,24 +2,27 @@
 ----------------------------------------------
  _  _________   ______ _____ ___  _   _ _____ 
 | |/ / ____\ \ / / ___|_   _/ _ \| \ | | ____|
-| ' /|  _|  \ V /\___ \ | || | | |  \| |  _|  
+| " /|  _|  \ V /\___ \ | || | | |  \| |  _|  
 | . \| |___  | |  ___) || || |_| | |\  | |___ 
 |_|\_\_____| |_| |____/ |_| \___/|_| \_|_____|
 ----------------------------------------------                                               
-           Developer Utility Library
-                   V1.0.9             
+               Utility Library
+                   V1.1.0              
 ----------------------------------------------
 ]]
 
-fx_version 'cerulean'
-games { 'gta5', 'rdr3' }
+fx_version "cerulean"
+games { "gta5", "rdr3" }
 
-name 'fivem_utils'
-version '1.0.9'
-description 'Keystone - Developer Utility Library'
-author 'keystone'
-repository 'https://github.com/keystonehub/fivem_utils'
-lua54 'yes'
+name "keystone"
+version "1.1.0"
+description "Keystone - Utility Library with Common UI and Bridges"
+author "Case"
+repository "https://github.com/keystonehub/key_utils"
+lua54 "yes"
+
+fx_version "cerulean"
+game "gta5"
 
 ui_page 'ui/index.html'
 nui_callback_strict_mode 'true'
@@ -28,24 +31,22 @@ files {
     'ui/**'
 }
 
-shared_script 'config.lua'
-shared_script 'init.lua'
-server_script 'user_registry.lua' -- Required! user accounts handle permissions for commands etc.. dont remove.
+server_script "@oxmysql/lib/MySQL.lua" -- OxMySQL
+
+shared_script "init.lua"
+server_script "users.lua" -- Required! user accounts handle permissions for commands etc.. dont remove.
+
+-- shared_script "@ox_lib/init.lua" -- Uncomment if using ox_lib or ox_core
 
 shared_scripts {
-    'data/*',
-    'modules/**/shared.lua',
-}
-
-server_scripts {
-    --- oxmysql
-    '@oxmysql/lib/MySQL.lua',
-    'modules/**/server.lua',
-    'scripts/**/server.lua'
+    "lib/data/*.lua",
+    "lib/modules/**/*.lua",
+    "lib/bridges/**/*.lua",
+    "lib/events/*.lua",
+    "lib/threads/*.lua",
+    "test.lua"
 }
 
 client_scripts {
-    'modules/**/client.lua',
-    'scripts/**/client.lua',
     'ui/lua/*'
 }
