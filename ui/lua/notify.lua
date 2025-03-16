f1fc4f6e99395cc38c74bbc6e7a68ad83891f1b8
Case <90377400+CaseIRL@users.noreply.github.com>
2025-03-16 23:46:14 +0000

    Update notify.lua

--- ui/lua/notify.lua
+++ ui/lua/notify.lua
@@ -2,7 +2,7 @@
 
 --- Sends a notify.
 --- @param options table: The options for the notify.
-local function notify(options)
+function notify(options)
     SendNUIMessage({
         action = 'notify',
         type = options.type or 'info',
@@ -22,35 +22,3 @@ exports('notify', notify)
 RegisterNetEvent('key_utils:cl:notify', function(options)
     notify(options)
 end)
-
---- @section Testing
-
-RegisterCommand('ui:test_notify_bridge', function()
-    local test_notifications = {
-        { type = 'success', header = 'Success', message = 'This is a success notification.' },
-        { type = 'error', header = 'Error', message = 'This is an error notification.' },
-        { type = 'info', header = 'Info', message = 'This is an info notification.' },
-        { type = 'warning', header = 'Warning', message = 'This is a warning notification.' },
-    }
-    for i, notif in ipairs(test_notifications) do
-        SetTimeout(i * 2000, function()
-            exports.key_utils:send_notification(notif)
-        end)
-    end
-end, false)
-
-
---- @section Testing
-RegisterCommand('ui:test_notify', function()
-    local types = { 'success', 'error', 'info', 'warning', 'primary', 'secondary', 'light', 'dark', 'critical', 'neutral' }
-    for i, notif_type in ipairs(types) do
-        SetTimeout(i * 2000, function()
-            notify({
-                type = notif_type,
-                header = string.upper(notif_type),
-                message = 'This is a '.. notif_type .. ' notification example.',
-                duration = 5000
-            })
-        end)
-    end
-end, false)
